85c93416b37f9a5058bcf46f22e08d9a838cb7e9
Dmitriy <dimapavlov19dp99@gmail.com>
2022-09-11 16:27:47 +0300

    fix anim

--- js/animJquery.js
+++ js/animJquery.js
@@ -1,5 +1,5 @@
 
 $(function () {
-  $('.main_name, .main_text, .main_buttons, .main_sponsored').hide().slideDown(2000);
+  $('.main_name, .main_buttons, .main_sponsored').hide().slideDown(2000);
   $('.main_photo-settings').hide().fadeTo(2000,1)
 });

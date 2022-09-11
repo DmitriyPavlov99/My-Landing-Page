b3b95d06f2787269f3578500b7f29d6381fc7879
Dmitriy <dimapavlov19dp99@gmail.com>
2022-09-11 22:15:47 +0300

    fix anim

--- js/animJquery.js
+++ js/animJquery.js
@@ -3,6 +3,11 @@ $(function () {
   setTimeout(startAnimMain,0)
 });
 function startAnimMain() {
-    $('.main_name, .main_buttons, .main_sponsored').hide().slideDown(2000);
-    $('.main_photo-settings').hide().fadeTo(2000,1)
+    
+  $('.main_photo-settings').hide().fadeTo(2000,1)
+    if($(window).width() > 750) {
+      $('.main_name, .main_text, .main_buttons, .main_sponsored').hide().slideDown(2000).fadeTo(2000,1);
+    } else if ($(window).width() < 750) {
+      $('.main_name, .main_buttons, .main_sponsored').hide().slideDown(2000).fadeTo(2000,1);
+    }
 }
